949e3aeda7af2840675c3eb8d6f74057ce98f9e5
Matthias Clasen <mclasen@redhat.com>
2016-06-27 23:47:02 -0400

    Add license headers

--- appchooserdialog.h
+++ appchooserdialog.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright © 2016 Red Hat, Inc
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *       Matthias Clasen <mclasen@redhat.com>
+ */
+
+#pragma once
+
 #include <gtk/gtk.h>
 
 G_DECLARE_FINAL_TYPE (AppChooserDialog, app_chooser_dialog, APP, CHOOSER_DIALOG, GtkWindow)
@@ -8,4 +30,5 @@ AppChooserDialog * app_chooser_dialog_new (const char **app_ids,
                                            const char *accept_label,
                                            const char *title,
                                            const char *heading);
-void app_chooser_dialog_set_selected (AppChooserDialog *dialog, const char *choice_id);
+void app_chooser_dialog_set_selected (AppChooserDialog *dialog,
+                                      const char *choice_id);
