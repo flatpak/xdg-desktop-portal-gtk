109b4a0992a6ab56b2493b23046d6b01fb84e161
Matthias Clasen <mclasen@redhat.com>
2017-03-31 06:07:27 -0400

    Allow setting the default in testappchooser
    This will let us test handling of the default choice.

--- src/testappchooser.c
+++ src/testappchooser.c
@@ -14,21 +14,27 @@ done_cb (GtkWidget *dialog,
         gtk_main_quit ();
 }
 
+
 int
 main (int argc, char *argv[])
 {
         GtkWidget *window;
         const char **apps;
         int i;
+        const char *default_id = NULL;
+        GOptionEntry entries[] = {
+          { "default", 0, 0, G_OPTION_ARG_STRING, &default_id, "The default choice", "ID" },
+          { NULL, }
+        };
 
-        gtk_init (NULL, NULL);
+        gtk_init_with_args (&argc, &argv, "APP...", entries, NULL, NULL);
 
         apps = g_new (const char *, argc);
         for (i = 0; i + 1 < argc; i++)
                 apps[i] = argv[i + 1];
         apps[argc - 1] = NULL;
 
-        window = GTK_WIDGET (app_chooser_dialog_new (apps, NULL, "Cancel", "Select", "Open With", "A heading goes here"));
+        window = GTK_WIDGET (app_chooser_dialog_new (apps, default_id, "Cancel", "Select", "Open With", "A heading goes here"));
         g_signal_connect (window, "done", G_CALLBACK (done_cb), NULL);
 
         gtk_widget_show (window);
