0fd6783afedb63935af94d8d88f082cae61c06d2
Matthias Clasen <mclasen@redhat.com>
2017-05-29 11:17:28 -0400

    Use a shorter name for apps
    Some still break the grid, such as the GIMP.

--- src/appchooserrow.c
+++ src/appchooserrow.c
@@ -85,8 +85,7 @@ app_chooser_row_new (GAppInfo *info)
                                 GTK_ICON_SIZE_DIALOG);
       gtk_image_set_pixel_size (GTK_IMAGE (row->icon), 48);
     }
-  gtk_label_set_label (GTK_LABEL (row->name),
-                       g_app_info_get_display_name (info));
+  gtk_label_set_label (GTK_LABEL (row->name), g_app_info_get_name (info));
 
   return row;
 }
