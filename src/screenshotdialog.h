03d3a932689f5abdb1fbfa2f7dc4cc5560419587
Matthias Clasen <mclasen@redhat.com>
2017-12-16 21:44:21 -0500

    Add options to the screenshot portal
    Similar to gnome-screenshot --interactive, expose options
    for window and area screenshots.

--- src/screenshotdialog.h
+++ src/screenshotdialog.h
@@ -1,10 +1,12 @@
 #include <gtk/gtk.h>
+#include "shell-dbus.h"
 
 #define SCREENSHOT_TYPE_DIALOG (screenshot_dialog_get_type ())
-#define SCREENSHOT_DIALOG(object) (G_TYPE_CHECK_INSTANCE_CAST ((object, SCREENSHOT_TYPE_DIALOG, ScreenshotDialog)))
+#define SCREENSHOT_DIALOG(object) (G_TYPE_CHECK_INSTANCE_CAST (object, SCREENSHOT_TYPE_DIALOG, ScreenshotDialog))
 
 typedef struct _ScreenshotDialog ScreenshotDialog;
 typedef struct _ScreenshotDialogClass ScreenshotDialogClass;
 
 ScreenshotDialog * screenshot_dialog_new (const char *app_id,
-                                          const char *filename);
+                                          gboolean interactive,
+                                          OrgGnomeShellScreenshot *shell);
