flatpak/xdg-desktop-portal-gtk

commit 68c044c2c97261baa9b154ad29ab3ec826dd26b4

Use the application name in the print notification (#304)
When printing with an flatpak application, the notification
telling that it prints says "Document from %s" where %s is the
app desktop file id (i.e. will print "Document from
org.gnome.Evince" with evince).

This is not very clear what this name is in the notification,
particularly with a third-party app, for example
com.github.my_beautiful_github_name.app_name.

So this changes %s to the actual translated application name,
looking in the corresponding desktop file entry.

src/print.c
@@ -31,6 +31,7 @@
 #include <gio/gunixfdlist.h>
 #include <gio/gunixinputstream.h>
 #include <gio/gunixoutputstream.h>
+#include <gio/gdesktopappinfo.h>
 
 #include "xdg-desktop-portal-dbus.h"
 
@@ -259,8 +260,27 @@ print_file (int fd,
   g_autoptr(GUnixInputStream) istream = NULL;
   g_autoptr(GUnixOutputStream) ostream = NULL;
   int fd2;
+  g_autofree char *app_desktop_fullname = NULL;
+  g_autoptr (GDesktopAppInfo) app_info = NULL;
+  // ensures the app_name won't be NULL even if the app_id.desktop file doesn't exist
+  g_autofree char *app_name = g_strdup (app_id);
 
-  title = g_strdup_printf ("Document from %s", app_id);
+  if (!g_str_has_suffix (app_id, ".desktop"))
+    {
+      app_desktop_fullname = g_strconcat (app_id, ".desktop", NULL);
+    }
+  else
+    {
+      app_desktop_fullname = g_strdup (app_id);
+    }
+
+  app_info = g_desktop_app_info_new (app_desktop_fullname);
+  if (app_info)
+    {
+      app_name = g_desktop_app_info_get_locale_string (app_info, "Name");
+    }
+
+  title = g_strdup_printf ("Document from %s", app_name);
 
   if (!preview)
     job = gtk_print_job_new (title, printer, settings, page_setup);

commit c8c2285de0e6148ea29ed0472b950a1ce0e349ee

Don't leak app_name string if a desktop file or it's "Name" is absent
Note that earlier the fallback app_name may or may not have included
the .desktop extension as a suffix, depending on what was passed as
app_id to the function.  Now, it will always include the extension.

As a fallback, this shouldn't matter and being more predictable is
likely a good thing.

Fallout from 68c044c2c97261baa9b154ad29ab3ec826dd26b4

src/print.c
@@ -263,7 +263,7 @@ print_file (int fd,
   g_autofree char *app_desktop_fullname = NULL;
   g_autoptr (GDesktopAppInfo) app_info = NULL;
   // ensures the app_name won't be NULL even if the app_id.desktop file doesn't exist
-  g_autofree char *app_name = g_strdup (app_id);
+  g_autofree char *app_name = NULL;
 
   if (!g_str_has_suffix (app_id, ".desktop"))
     {
@@ -279,6 +279,9 @@ print_file (int fd,
     {
       app_name = g_desktop_app_info_get_locale_string (app_info, "Name");
     }
+
+  if (app_name == NULL)
+    app_name = g_strdup (app_id);
 
   title = g_strdup_printf ("Document from %s", app_name);
 