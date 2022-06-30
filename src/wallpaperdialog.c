flatpak/xdg-desktop-portal-gtk

commit 1d1302d70603710ddf1ec353a7ad9af80fdec057

wallpaper: Cache remote images (#260)
Fixes #254

src/wallpaperdialog.c
@@ -38,6 +38,7 @@ struct _WallpaperDialog {
   WallpaperPreview *lockscreen_preview;
 
   gchar *picture_uri;
+  SetWallpaperOn set_on;
 };
 
 struct _WallpaperDialogClass {
@@ -133,35 +134,70 @@ set_wallpaper_on_lockscreen (WallpaperDialog *self)
                                GTK_WIDGET (self->lockscreen_preview));
 }
 
+static void
+on_image_loaded_cb (GObject *source_object,
+                    GAsyncResult *result,
+                    gpointer data)
+{
+  WallpaperDialog *self = data;
+  GFileIOStream *stream = NULL;
+  GFile *image_file = G_FILE (source_object);
+  GFile *tmp = g_file_new_tmp ("XXXXXX", &stream, NULL);
+  g_autoptr(GError) error = NULL;
+  gchar *contents = NULL;
+  gsize length = 0;
+
+  g_object_unref (stream);
+
+  if (!g_file_load_contents_finish (image_file, result, &contents, &length, NULL, &error))
+    {
+      g_warning ("Failed to load image: %s", error->message);
+
+      return;
+    }
+
+  g_file_replace_contents (tmp, contents, length, NULL, FALSE, G_FILE_CREATE_REPLACE_DESTINATION, NULL, NULL, &error);
+
+  self->picture_uri = g_strdup (g_file_get_uri (tmp));
+
+  switch (self->set_on)
+    {
+      case BACKGROUND:
+        set_wallpaper_on_background (self);
+        break;
+      case LOCKSCREEN:
+        set_wallpaper_on_lockscreen (self);
+        break;
+      default:
+        set_wallpaper_on_background (self);
+        set_wallpaper_on_lockscreen (self);
+
+        gtk_window_set_title (GTK_WINDOW (self),
+                              _("Set Background & Lock Screen"));
+    }
+}
+
 WallpaperDialog *
 wallpaper_dialog_new (const gchar *picture_uri,
                       const gchar *app_id,
                       SetWallpaperOn set_on)
 {
   WallpaperDialog *self;
+  g_autoptr(GFile) image_file = g_file_new_for_uri (picture_uri);
 
   self = g_object_new (wallpaper_dialog_get_type (), NULL);
 
-  if (picture_uri)
-    {
-      self->picture_uri = g_strdup (picture_uri);
-
-      switch (set_on)
-        {
-          case BACKGROUND:
-            set_wallpaper_on_background (self);
-            break;
-          case LOCKSCREEN:
-            set_wallpaper_on_lockscreen (self);
-            break;
-          default:
-            set_wallpaper_on_background (self);
-            set_wallpaper_on_lockscreen (self);
-
-            gtk_window_set_title (GTK_WINDOW (self),
-                                  _("Set Background & Lock Screen"));
-        }
-    }
+  self->set_on = set_on;
+  g_file_load_contents_async (image_file,
+                              NULL,
+                              on_image_loaded_cb,
+                              self);
 
   return self;
+}
+
+const gchar *
+wallpaper_dialog_get_uri (WallpaperDialog *dialog)
+{
+  return dialog->picture_uri;
 }

src/wallpaper.c
@@ -88,33 +88,46 @@ on_file_copy_cb (GObject *source_object,
                  gpointer data)
 {
   WallpaperDialogHandle *handle = data;
+  g_autoptr(GFile) destination = NULL;
   GFile *picture_file = G_FILE (source_object);
   g_autoptr(GError) error = NULL;
   g_autofree gchar *uri = NULL;
+  g_autofree gchar *dest_path = NULL;
+  gchar *contents = NULL;
+  gsize length = 0;
 
   handle->response = 2;
 
   uri = g_file_get_uri (picture_file);
-  if (!g_file_copy_finish (picture_file, result, &error))
+  if (!g_file_load_contents_finish (picture_file, result, &contents, &length, NULL, &error))
     {
       g_warning ("Failed to copy '%s': %s", uri, error->message);
 
       goto out;
     }
 
+  destination = g_file_new_for_path (handle->picture_uri);
+  g_file_replace_contents (destination,
+                           contents,
+                           length,
+                           NULL, FALSE,
+                           G_FILE_CREATE_REPLACE_DESTINATION,
+                           NULL, NULL,
+                           &error);
+
   switch (handle->set_on)
     {
       case BACKGROUND:
-        if (set_gsettings (BACKGROUND_SCHEMA, uri))
+        if (set_gsettings (BACKGROUND_SCHEMA, handle->picture_uri))
           handle->response = 0;
         break;
       case LOCKSCREEN:
-        if (set_gsettings (LOCKSCREEN_SCHEMA, uri))
+        if (set_gsettings (LOCKSCREEN_SCHEMA, handle->picture_uri))
           handle->response = 0;
         break;
       default:
-        if (set_gsettings (BACKGROUND_SCHEMA, uri) &&
-            set_gsettings (LOCKSCREEN_SCHEMA, uri))
+        if (set_gsettings (BACKGROUND_SCHEMA, handle->picture_uri) &&
+            set_gsettings (LOCKSCREEN_SCHEMA, handle->picture_uri))
           handle->response = 0;
     }
 
@@ -123,12 +136,11 @@ out:
 }
 
 static void
-set_wallpaper (WallpaperDialogHandle *handle)
+set_wallpaper (WallpaperDialogHandle *handle,
+               const gchar *uri)
 {
   g_autofree gchar *dest_filename = NULL;
-  g_autofree gchar *dest_path = NULL;
   g_autoptr(GFile) source = NULL;
-  g_autoptr(GFile) destination = NULL;
 
   switch (handle->set_on)
     {
@@ -142,18 +154,13 @@ set_wallpaper (WallpaperDialogHandle *handle)
         dest_filename = g_strdup ("both");
     }
 
-  dest_path = g_build_filename (g_get_user_config_dir (), dest_filename, NULL);
-  source = g_file_new_for_uri (handle->picture_uri);
-  destination = g_file_new_for_path (dest_path);
+  handle->picture_uri = g_build_filename (g_get_user_config_dir (), dest_filename, NULL);
 
-  g_file_copy_async (source,
-                     destination,
-                     G_FILE_COPY_OVERWRITE,
-                     G_PRIORITY_DEFAULT,
-                     NULL, NULL, NULL,
-                     on_file_copy_cb,
-                     handle);
-
+  source = g_file_new_for_uri (uri);
+  g_file_load_contents_async (source,
+                              NULL,
+                              on_file_copy_cb,
+                              handle);
 }
 
 static void
@@ -176,7 +183,7 @@ handle_wallpaper_dialog_response (WallpaperDialog *dialog,
         break;
       case GTK_RESPONSE_APPLY:
         handle->response = 0;
-        set_wallpaper (handle);
+        set_wallpaper (handle, wallpaper_dialog_get_uri (dialog));
 
         return;
     }
@@ -214,7 +221,6 @@ handle_set_wallpaper_uri (XdpImplWallpaper *object,
   handle->impl = object;
   handle->invocation = invocation;
   handle->request = g_object_ref (request);
-  handle->picture_uri = g_strdup (arg_uri);
 
   if (!set_on || g_strcmp0 (set_on, "both") == 0)
     handle->set_on = BOTH;
@@ -225,7 +231,7 @@ handle_set_wallpaper_uri (XdpImplWallpaper *object,
 
   if (!show_preview)
     {
-      set_wallpaper (handle);
+      set_wallpaper (handle, g_strdup (arg_uri));
       goto out;
     }
 

src/wallpaperdialog.h
@@ -39,3 +39,5 @@ GType             wallpaper_dialog_get_type (void) G_GNUC_CONST;
 WallpaperDialog * wallpaper_dialog_new (const char *picture_uri,
                                         const char *app_id,
                                         SetWallpaperOn set_on);
+
+const gchar     * wallpaper_dialog_get_uri (WallpaperDialog *dialog);

src/wallpaperpreview.ui
@@ -28,6 +28,16 @@
             <property name="can-focus">False</property>
             <property name="expand">True</property>
             <child>
+              <object class="GtkSpinner">
+                <property name="visible">True</property>
+                <property name="active">True</property>
+                <style>
+                  <class name="background"/>
+                </style>
+              </object>
+            </child>
+
+            <child>
               <object class="GtkFrame">
                 <property name="visible">True</property>
                 <property name="can-focus">False</property>

commit 10c8d0e9cbb91a4cac3679494f01b589dd5dff5d

wallpaperdialog: Don't leak the temporary GFile
Fallout from 1d1302d70603710ddf1ec353a7ad9af80fdec057

src/wallpaperdialog.c
@@ -119,7 +119,7 @@ on_image_loaded_cb (GObject *source_object,
   WallpaperDialog *self = data;
   GFileIOStream *stream = NULL;
   GFile *image_file = G_FILE (source_object);
-  GFile *tmp = g_file_new_tmp ("XXXXXX", &stream, NULL);
+  g_autoptr(GFile) tmp = g_file_new_tmp ("XXXXXX", &stream, NULL);
   g_autoptr(GError) error = NULL;
   gchar *contents = NULL;
   gsize length = 0;